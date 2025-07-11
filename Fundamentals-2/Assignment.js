9b245e67423c8e233a7f65b04991587c7fee2560
Kirtan Agravat <kirtan@weybee.in>
2025-07-11 11:24:17 +0530

    Assignment-2 fixed

--- Fundamentals-2/Assignment.js
+++ Fundamentals-2/Assignment.js
@@ -1,19 +1,19 @@
 //Assignment - 1
 // LECTURE: Functions
 
-// function describeCountry(country, population, capitalCity) {
-//   console.log(
-//     `${country} has ${population} million people and its capital city is ${capitalCity}`
-//   );
-// }
+function describeCountry(country, population, capitalCity) {
+  console.log(
+    `${country} has ${population} million people and its capital city is ${capitalCity}`
+  );
+}
 
-// const country1 = describeCountry("india", 100, "delhi");
-// const country2 = describeCountry("nepal", 10, "katmandu");
-// const country3 = describeCountry("sri lanka ", 100, "ri Jayawardenepura Kotte");
+const country1 = describeCountry("india", 100, "delhi");
+const country2 = describeCountry("nepal", 10, "katmandu");
+const country3 = describeCountry("sri lanka ", 100, "ri Jayawardenepura Kotte");
 
-// console.log(country1);
-// console.log(country2);
-// console.log(country3);
+console.log(country1);
+console.log(country2);
+console.log(country3);
 
 //Assignment - 2
 // LECTURE: Function Declarations vs. Expressions
@@ -22,41 +22,41 @@ function percentageOfWorld1(population) {
   return ((population / 7900) * 100).toFixed(2);
 }
 
-// percentageOfWorld1(1000);
-// percentageOfWorld1(5000);
-// percentageOfWorld1(1200);
+percentageOfWorld1(1000);
+percentageOfWorld1(5000);
+percentageOfWorld1(1200);
 
-// const percentageOfWorld2 = function (population) {
-//   console.log((population / 7900) * 100);
-// };
+const percentageOfWorld2 = function (population) {
+  console.log((population / 7900) * 100);
+};
 
-// percentageOfWorld2(1441);
-// percentageOfWorld2(1551);
-// percentageOfWorld2(1212);
+percentageOfWorld2(1441);
+percentageOfWorld2(1551);
+percentageOfWorld2(1212);
 
 // Assignment - 3
 // LECTURE: Arrow Functions
 
-// const percentageOfWorld3 = (population) => {
-//   console.log((population / 7900) * 100);
-// };
+const percentageOfWorld3 = (population) => {
+  console.log((population / 7900) * 100);
+};
 
-// percentageOfWorld3(1212);
-// percentageOfWorld3(121);
-// percentageOfWorld3(1200);
+percentageOfWorld3(1212);
+percentageOfWorld3(121);
+percentageOfWorld3(1200);
 
 //Assignment - 4
 // LECTURE: Functions Calling Other Functions
 
-// function describePopulation(country, population) {
-//   console.log(
-//     `${country} has ${population} million people which is about ${percentageOfWorld1(
-//       population
-//     )}% of the world`
-//   );
-// }
+function describePopulation(country, population) {
+  console.log(
+    `${country} has ${population} million people which is about ${percentageOfWorld1(
+      population
+    )}% of the world`
+  );
+}
 
-// describePopulation("india", 1002);
+describePopulation("india", 1002);
 
 //Assignment - 5
 //LECTURE: Introduction to Arrays
@@ -65,7 +65,7 @@ const populations = [1463, 1416, 347, 285];
 
 // India (1463 million), China (1416 million), United States (347 million), and Indonesia (285 million)
 
-// console.log(populations.length == 4);
+console.log(populations.length == 4);
 
 const percentage = [
   percentageOfWorld1(populations[0]),
@@ -110,12 +110,9 @@ console.log(myCountry);
 //Assignment - 8
 //LECTURE: Dot vs. Bracket Notation
 
-// console.log(
-//   `${myCountry.country} has ${myCountry.population} millions ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
-// );
-
-// Finland has 6 million finnish-speaking people, 3 neighbouring countries
-// and a capital called Helsinki
+console.log(
+  `${myCountry.country} has ${myCountry.population} millions ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
+);
 
 myCountry.population -= 2;
 
@@ -140,9 +137,9 @@ myCountry.checkIsland = function () {
 //Assignment - 10
 //LECTURE: Iteration: The for Loop
 
-// for (let i = 1; i <= 50; i++) {
-//   console.log("Voter number " + i + " is currently voting");
-// }
+for (let i = 1; i <= 50; i++) {
+  console.log("Voter number " + i + " is currently voting");
+}
 
 //Assignment - 11
 //LECTURE: Looping Arrays, Breaking and Continuing
